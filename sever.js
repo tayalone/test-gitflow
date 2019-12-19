ab06a84c78fb1aac62309f76259fb4d2d1fb7065
Naphuc94 <naphuc94@gmail.com>
2019-12-19 13:23:13 +0700

    Add new route name testFlowTesting

--- sever.js
+++ sever.js
@@ -1,11 +1,15 @@
-const express = require('express');
-const app = express();
-const port = 8888;
+const express = require('express')
+const app = express()
+const port = 8888
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Hello World!'))
 
 app.get('/testRequest', (req, res) => {
-  return res.send({ message: 'OK' });
-});
+  return res.send({ message: 'OK' })
+})
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.get('/testFlowTesting', (req, res) => {
+  return res.send('Hello World')
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
